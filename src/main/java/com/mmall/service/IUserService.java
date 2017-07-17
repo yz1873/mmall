bcf9a991e18d3b4268d62f2586a71eef8e58dd64
yz1873 <yz1873@126.com>
2017-07-17 17:02:56 +0800

    ServerResponse

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,8 @@
+package com.mmall.service;
+
+/**
+ * Created by Zhang Yu on 2017/7/17.
+ */
+public interface IUserService {
+    Object login(String username, String password);
+}
---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.mmall.service.impl;
+
+import com.mmall.service.IUserService;
+
+/**
+ * Created by Zhang Yu on 2017/7/17.
+ */
+public class UserServiceImpl implements IUserService {
+    @Override
+    public Object login(String username, String password) {
+        return null;
+    }
+}
