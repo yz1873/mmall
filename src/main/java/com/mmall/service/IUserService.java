283e777ba678aec350cd63cc734dfe286b0ffc65
yz1873 <yz1873@126.com>
2017-08-14 17:10:13 +0800

    5.3

--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -12,4 +12,8 @@ public interface IUserService {
     ServerResponse<String> register(User user);
 
     ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
 }
